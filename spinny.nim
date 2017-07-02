c6e66d7d9c67b2c0d8da712a980265700aa02210
Mark Molnar <molnarpw@gmail.com>
2017-07-02 19:16:17 +0200

    implemented basic spinner

--- spinny.nim
+++ spinny.nim
@@ -1,4 +1,4 @@
-import locks, terminal, os, threadpool, asyncdispatch, colorize, json
+import locks, terminal, os, threadpool, asyncdispatch, colorize, json, sequtils
 
 type
   Spinny = ref SpinnyObj
@@ -7,24 +7,28 @@ type
     lock: Lock
     text: string
     running: bool
+    spinnerData: JsonNode
 
-proc newSpinny*(text: string): Spinny =
+proc newSpinny*(text: string, spinner: string): Spinny =
   var spinners = readFile("spinners.json")
-  var spinnersJson = parseJson(spinners)
-  result = Spinny(text: text, running: true)
+  var spinnersJson = parseJson($spinners)
+  result = Spinny(text: text, running: true, spinnerData: spinnersJson[spinner])
 
 
 proc spinnyLoop(spinny: Spinny) {.thread.} =
+  var frameCounter = 0
 
   while spinny.running:
+    var spinner = spinny.spinnerData["frames"][frameCounter].getStr()
     acquire(spinny.lock)
     stdout.write("\r")
-    stdout.write(spinny.text)
+    stdout.write(spinner & " " & spinny.text)
     stdout.flushFile()
     release(spinny.lock)
     sleep(80)
     eraseLine()
 
+    if frameCounter >= spinny.spinnerData["frames"].len - 1: frameCounter = 0 else: frameCounter += 1
 
 proc start*(spinny: Spinny) =
   initLock(spinny.lock)
@@ -36,7 +40,7 @@ proc stop(spinny: Spinny) =
   echo ""
   setCursorXPos(0)
 
-var sp = newSpinny("Loading something..".bold.fgRed)
+var sp = newSpinny("Loading something..".bold.fgRed, "dots2")
 sp.start()
 
 for x in countup(5, 10):

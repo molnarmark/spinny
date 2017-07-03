de44fd975ee22ea4794b9090796f12c35caab5e8
Mark Molnar <molnarpw@gmail.com>
2017-07-03 20:24:41 +0200

    added success & error stopping procs

--- spinny.nim
+++ spinny.nim
@@ -9,35 +9,65 @@ type
     running: bool
     frames: seq[JsonNode]
     frame: string
+    customSymbol: bool
 
   SpinnyEvent = object
     name: string
     payload: string
 
-var c: Channel[SpinnyEvent]
+var spinnyChannel: Channel[SpinnyEvent]
 
 proc newSpinny*(text: string, spinner: string): Spinny =
   var spinners = readFile("spinners.json")
   var frames = parseJson($spinners)[spinner]["frames"].getElems()
-  result = Spinny(text: text, running: true, frames: frames)
+  result = Spinny(text: text, running: true, frames: frames, customSymbol: false)
 
-proc setColor*(spinny: Spinny, color: proc(x: string): string) =
+proc setSymbolColor*(spinny: Spinny, color: proc(x: string): string) =
   spinny.frames = map(spinny.frames, proc(node: JsonNode): JsonNode = newJString(node.getStr().color()))
 
+proc setSymbol*(spinny: Spinny, newSymbol: string) =
+  spinnyChannel.send(SpinnyEvent(name: "symbol_change", payload: newSymbol))
+
+proc setText*(spinny: Spinny, newSymbol: string) =
+  spinnyChannel.send(SpinnyEvent(name: "text_change", payload: newSymbol))
+
 proc handleEvent(spinny: Spinny, eventData: SpinnyEvent): bool =
-  if eventData.name == "stop": return false
+  case eventData.name:
+  of "stop":
+    return false
+
+  of "symbol_change":
+    spinny.customSymbol = true
+    spinny.frame = eventData.payload
+    return true
+
+  of "text_change":
+    spinny.text = eventData.payload
+    return true
+
+  of "stop_success":
+    spinny.customSymbol = true
+    spinny.frame = "✔".bold.fgGreen
+    spinny.text = eventData.payload.bold.fgGreen
+    return true
+
+  of "stop_error":
+    spinny.customSymbol = true
+    spinny.frame = "✖".bold.fgRed
+    spinny.text = eventData.payload.bold.fgRed
+    return true
 
 proc spinnyLoop(spinny: Spinny) {.thread.} =
   var frameCounter = 0
 
   while spinny.running:
-    var data = c.tryRecv()
+    var data = spinnyChannel.tryRecv()
     if data.dataAvailable:
       if not spinny.handleEvent(data.msg):
         break
 
     stdout.flushFile()
-    spinny.frame = spinny.frames[frameCounter].getStr()
+    if not spinny.customSymbol: spinny.frame = spinny.frames[frameCounter].getStr()
     acquire(spinny.lock)
     eraseLine()
     stdout.write(spinny.frame & " " & spinny.text)
@@ -50,15 +80,24 @@ proc spinnyLoop(spinny: Spinny) {.thread.} =
     else:
       frameCounter += 1
 
+
 proc start*(spinny: Spinny) =
   initLock(spinny.lock)
-  c.open()
+  spinnyChannel.open()
   createThread(spinny.t, spinnyLoop, spinny)
 
 proc stop*(spinny: Spinny) =
   spinny.running = false
-  c.send(SpinnyEvent(name: "stop", payload: ""))
+  spinnyChannel.send(SpinnyEvent(name: "stop", payload: ""))
   joinThread(spinny.t)
   echo ""
-  # c.close()
+  # spinnyChannel.close()
   sync()
+
+proc success*(spinny: Spinny, msg: string) =
+  spinnyChannel.send(SpinnyEvent(name: "stop_success", payload: msg))
+  spinny.stop()
+
+proc error*(spinny: Spinny, msg: string) =
+  spinnyChannel.send(SpinnyEvent(name: "stop_error", payload: msg))
+  spinny.stop()
--- spinny_example.nim
+++ spinny_example.nim
@@ -1,21 +1,11 @@
 import spinny, colorize, os
 
-var sp = newSpinny("Loading something..".bold.fgRed, "dots")
-sp.setColor(colorize.fgRed)
-sp.start()
+var spinner = newSpinny("Loading my life..".fgWhite, "dots")
+spinner.setSymbolColor(colorize.fgBlue)
+spinner.start()
 
 for x in countup(5, 10):
   sleep(500)
   # doing work
 
-sp.stop()
-
-var sp2 = newSpinny("Loading ffff..".bold.fgYellow, "dots5")
-sp2.setColor(colorize.fgRed)
-sp2.start()
-
-for x in countup(5, 10):
-  sleep(500)
-  # doing work
-
-sp2.stop()
+spinner.success("Everything was downloaded.")

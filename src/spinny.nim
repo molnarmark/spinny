c84bec2e1edfcf8c28e0df83b538c982ada178d6
Danil Yarantsev <21169548+Yardanico@users.noreply.github.com>
2020-05-19 01:46:01 +0300

    Don't use threadpool, json, simplify code

---
+++ src/spinny.nim
@@ -0,0 +1,138 @@
+import os, terminal, locks
+import sequtils
+
+import colorize
+import spinners
+export colorize, spinners.SpinnerKind, spinners.Spinner
+
+type
+  Spinny = ref object
+    t: Thread[Spinny]
+    lock: Lock
+    text: string
+    running: bool
+    frames: seq[string]
+    frame: string
+    interval: int
+    customSymbol: bool
+
+  EventKind = enum
+    Stop, StopSuccess, StopError,
+    SymbolChange, TextChange,
+
+  SpinnyEvent = object
+    kind: EventKind
+    payload: string
+
+var spinnyChannel: Channel[SpinnyEvent]
+
+proc newSpinny*(text: string, s: Spinner): Spinny =
+  Spinny(
+    text: text,
+    running: true,
+    frames: s.frames,
+    customSymbol: false,
+    interval: s.interval
+  )
+
+proc newSpinny*(text: string, spinType: SpinnerKind): Spinny =
+  newSpinny(text, Spinners[spinType])
+
+proc setSymbolColor*(spinny: Spinny, color: proc(x: string): string) =
+  spinny.frames = mapIt(spinny.frames, color(it))
+
+proc setSymbol*(spinny: Spinny, newSymbol: string) =
+  spinnyChannel.send(SpinnyEvent(kind: SymbolChange, payload: newSymbol))
+
+proc setText*(spinny: Spinny, newSymbol: string) =
+  spinnyChannel.send(SpinnyEvent(kind: TextChange, payload: newSymbol))
+
+proc handleEvent(spinny: Spinny, eventData: SpinnyEvent): bool =
+  result = true
+  case eventData.kind
+  of Stop:
+    result = false
+  of SymbolChange:
+    spinny.customSymbol = true
+    spinny.frame = eventData.payload
+  of TextChange:
+    spinny.text = eventData.payload
+  of StopSuccess:
+    spinny.customSymbol = true
+    spinny.frame = "✔".bold.fgGreen
+    spinny.text = eventData.payload.bold.fgGreen
+  of StopError:
+    spinny.customSymbol = true
+    spinny.frame = "✖".bold.fgRed
+    spinny.text = eventData.payload.bold.fgRed
+
+proc spinnyLoop(spinny: Spinny) {.thread.} =
+  var frameCounter = 0
+
+  while spinny.running:
+    let data = spinnyChannel.tryRecv()
+    if data.dataAvailable:
+      # If we received a Stop event
+      if not spinny.handleEvent(data.msg):
+        spinnyChannel.close()
+        # This is required so we can reopen the same channel more than once
+        # See https://github.com/nim-lang/Nim/issues/6369
+        spinnyChannel = default(typeof(spinnyChannel))
+        # TODO: Do we need spinny.running at all?
+        spinny.running = false
+        break
+
+    stdout.flushFile()
+    if not spinny.customSymbol:
+      spinny.frame = spinny.frames[frameCounter]
+
+    withLock spinny.lock:
+      eraseLine()
+      stdout.write(spinny.frame & " " & spinny.text)
+      stdout.flushFile()
+
+    sleep(spinny.interval)
+
+    if frameCounter >= spinny.frames.len - 1:
+      frameCounter = 0
+    else:
+      frameCounter += 1
+
+proc start*(spinny: Spinny) =
+  initLock(spinny.lock)
+  spinnyChannel.open()
+  createThread(spinny.t, spinnyLoop, spinny)
+
+proc stop*(spinny: Spinny, kind = Stop, payload = "") =
+  spinnyChannel.send(SpinnyEvent(kind: kind, payload: payload))
+  spinnyChannel.send(SpinnyEvent(kind: Stop))
+  joinThread(spinny.t)
+  echo ""
+
+proc success*(spinny: Spinny, msg: string) =
+  spinny.stop(StopSuccess, msg)
+
+proc error*(spinny: Spinny, msg: string) =
+  spinny.stop(StopError, msg)
+
+when isMainModule:
+  var spinner1 = newSpinny("Loading file..".fgWhite, Dots)
+  spinner1.setSymbolColor(colorize.fgBlue)
+  spinner1.start()
+
+  # Do some 'work'
+  for _ in 0 .. 10:
+    sleep(200)
+
+  spinner1.success("File was loaded successfully.")
+
+  var spinner2 = newSpinny("Downloading files..".fgYellow, Clock)
+
+  spinner2.setSymbolColor(colorize.fgLightBlue)
+  spinner2.start()
+
+  # Do some 'work'
+  for _ in 0 .. 10:
+    sleep(200)
+
+  spinner2.error("Sorry, something went wrong during downloading!")
